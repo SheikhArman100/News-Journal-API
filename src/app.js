42b158ab9e1d74b7df98cac0471722de1aec4440
SheikhArman100 <sheikharman100@gmail.com>
2023-08-23 17:37:15 +0600

    cron added again

--- src/app.js
+++ src/app.js
@@ -5,10 +5,21 @@ import mongoConnect from "./utils/mongoConnect.js";
 
 import cors from "cors";
 import routes from "./route.js";
+import cron from "node-cron";
+import { makeCronJobRequest } from "./utils/cron-jobs.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+cron.schedule("0 */6 * * *", async () => {
+    try {
+      console.log("Running scheduled task...");
+      await makeCronJobRequest();
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 
 const port = process.env.PORT;
 app.listen(port, async () => {
---
+++ src/utils/cron-jobs.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export async function makeCronJobRequest() {
+  const base_url = process.env.HOST_URL;
+
+  try {
+    await axios.post(`${base_url}/api/headline`);
+    await axios.post(`${base_url}/api/news`);
+    await axios.post(`${base_url}/api/culture`);
+    await axios.post(`${base_url}/api/sports`);
+    await axios.post(`${base_url}/api/lifestyle`);
+    await axios.post(`${base_url}/api/opinion`);
+    console.log("cron-job request successful");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
